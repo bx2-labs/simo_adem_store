8a0664654e03c5763f12e0615a505e7e683f37c5
BX2 <mazariadam88@gmail.com>
2026-06-07 15:37:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { inject } from "@vercel/analytics"; // 1. زدنا الاستدعاء هنا
+
+// 2. شعلنا التحليلات
+inject();
 
 createRoot(document.getElementById("root")!).render(<App />);
